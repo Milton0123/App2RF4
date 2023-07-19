890a767d207d2c3f95b191f20f4ec7d6e0302f72
Emilce Cano <emilce.cano95@gmail.com>
2023-07-19 01:04:15 -0300

    create layout sell and binding

---
+++ app/src/main/java/com/example/app2rf4/SellActivity.kt
@@ -0,0 +1,16 @@
+package com.example.app2rf4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app2rf4.databinding.ActivitySellBinding
+
+class SellActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySellBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivitySellBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
