d561fbee560edab0fa1e0220fcc5d991fd6ba98d
Emilce Cano <emilce.cano95@gmail.com>
2023-07-20 00:40:11 -0300

    update

--- app/src/main/java/com/example/app2rf4/ui/LoginActivity.kt
+++ app/src/main/java/com/example/app2rf4/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.app2rf4.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app2rf4.HomeActivity
 import com.example.app2rf4.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -18,7 +19,7 @@ class LoginActivity : AppCompatActivity() {
     fun action() {
         binding.btEnterLogin.setOnClickListener {
             startActivity(
-                Intent(this, AddActivity::class.java)
+                Intent(this, HomeActivity::class.java)
             )
         }
     }
